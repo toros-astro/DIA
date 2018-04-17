36596ba40ebb2ac5abb9148e06c6fbf7ccb72d60
Martin Beroiz <martinberoiz@gmail.com>
2018-04-17 16:27:28 -0500

    Fixes minor bug

--- oisdiff.py
+++ oisdiff.py
@@ -7,7 +7,7 @@ def oisdifference():
     zero = 2 * np.ones((10, 10))
     one = 3 * np.ones((10, 10))
     result = 5 * np.ones((10, 10))
-    suma, greeting = oismodule.subtract(zero, one)
+    suma, greeting = oismodule.subtract(zero, one, 1, 1)
     is_it_zero = np.sum(suma - result) == 0.0
     print(greeting)
     print("Is it zero: {}".format(is_it_zero))
