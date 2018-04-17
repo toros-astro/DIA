47840b7be7377cfdcac91ea5dd1406d4cc152f75
Martin Beroiz <martinberoiz@gmail.com>
2018-04-17 16:55:48 -0500

    Routine commit. WIP

--- src/oismodule.c
+++ src/oismodule.c
@@ -9,33 +9,57 @@
 static PyObject *
 oismodule_subtract(PyObject *self, PyObject *args)
 {
-    PyArrayObject *np_image, *np_refimage;
-    int deg; // The degree of the varying polynomial for the kernel
-    int stamp_side; // The stamp side 
+    PyArrayObject *np_sciimage, *np_refimage, *np_xc, *np_yc;
+    int stamp_side;  // The stamp side 
+    int kernel_side; // The kernel side in pixels
+    int poly_deg; // The degree of the varying polynomial for the kernel
 
-    if (!PyArg_ParseTuple(args, "O!O!ii", &PyArray_Type, &np_image,
-            &PyArray_Type, &np_refimage,
-            &deg, &stamp_side)) {
+    if (!PyArg_ParseTuple(args, "O!O!0!0!iii",
+        &PyArray_Type, &np_sciimage,
+        &PyArray_Type, &np_refimage,
+        &PyArray_Type, &np_xc,
+        &PyArray_Type, &np_yc,
+        &kernel_side, &poly_deg, &stamp_side)) {
         return NULL;
     }
-    if (NULL == np_image) return NULL;
+    if (NULL == np_sciimage) return NULL;
     if (NULL == np_refimage) return NULL;
+    if (NULL == np_xc) return NULL;
+    if (NULL == np_yc) return NULL;
 
-    int n = np_image->dimensions[0];
-    int m = np_image->dimensions[1];
+    int n = np_sciimage->dimensions[0];
+    int m = np_sciimage->dimensions[1];
+    int nstars = np_xc->dimensions[0];
 
-    double* image = (double*)np_image->data;
-    double* refimage = (double*)np_refimage->data;
+    double* sciimage_data = (double*)np_sciimage->data;
+    double* refimage_data = (double*)np_refimage->data;
+    int* xc = (int *)np_xc->data;
+    int* yc = (int *)np_yc->data;
 
-    double* addition = (double*)malloc(n * m * sizeof(*addition));
-    for(int i = 0; i < n*m; i++) {
-        addition[i] = image[i] + refimage[i];
-    }
+    image img = {sciimage_data, n, m};
+    image ref = {refimage_data, n, m};
 
-    npy_intp add_dims[2] = {n, m};
-    PyObject* add = PyArray_SimpleNewFromData(2, add_dims, NPY_DOUBLE, addition);
+    int poly_dof = (poly_deg + 1) * (poly_deg + 2) / 2; // number of degrees of freedom (ind var's) for the polynomial modulation
+    int total_dof = kernel_side * kernel_side * poly_dof; // size of convolution matrix
+    double *M = (double*) calloc(sizeof(double), total_dof * total_dof);
+    double *b = (double*) calloc(sizeof(double), total_dof);
 
-    return Py_BuildValue("Os", add, "Hello from the other side");
+    make_matrix_system(ref, img, kernel_side / 2, stamp_side / 2, poly_deg, nstars, xc, yc, M, b);
+    free(sciimage_data);
+
+    double *xsol = (double*) malloc(sizeof(double) * total_dof);
+    // This will solve the system Mx = b and store x in xsol
+    solve_system(total_dof, M, b, xsol);
+    free(M);
+    free(b);
+    double *ruinedimg_data = (double*) calloc(sizeof(double), n * m);
+    var_convolve(kernel_side / 2, poly_deg, total_dof, xsol, n, refimage_data, ruinedimg_data);
+    free(refimage_data);
+
+    npy_intp con_dims[2] = {n, m};
+    PyObject* py_conv_img = PyArray_SimpleNewFromData(2, con_dims, NPY_DOUBLE, ruinedimg_data);
+
+    return Py_BuildValue("O", py_conv_img);
 }
 
 static PyMethodDef OISModuleMethods[] = {
