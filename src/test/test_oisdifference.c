b056a33e8769f0299e2187af51f71e2d76f16851
Martin Beroiz <martinberoiz@gmail.com>
2018-05-22 14:01:45 -0500

    Added some test files

--- src/test/test_oisdifference.c
+++ src/test/test_oisdifference.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "oisdifference.h"
 
 int main(void) {
-    printf("Hello World!\n");
+    FILE* fp;
+    int h = 100, w = 100;
+    double *img = (double *)malloc(h * w * sizeof(*img));
+    fp = fopen("sample_img_100.txt", "r");
+    if (fp == NULL) {
+        printf("Problem opening file sample_img_100.txt\n");
+        return EXIT_FAILURE;
+    }
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            fscanf(fp, "%lf", img + i * w + j);
+        }
+    }
+    fclose(fp);
+    double *ref = (double *)malloc(h * w * sizeof(*img));
+    fp = fopen("sample_ref_100.txt", "r");
+    if (fp == NULL) {
+        printf("Problem opening file sample_ref_100.txt\n");
+        return EXIT_FAILURE;
+    }
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            fscanf(fp, "%lf", ref + i * w + j);
+        }
+    }
+    fclose(fp);
+    
     return EXIT_SUCCESS;
 }
 
