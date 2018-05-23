fed6a111ce9b598a9042c74ca237a9606e33c942
Martin Beroiz <martinberoiz@gmail.com>
2018-05-23 16:25:57 -0500

    Added empty tests for python

---
+++ tests/test_oisdiff.py
@@ -0,0 +1,12 @@
+import unittest
+import oisdiff
+import numpy as np
+
+
+class TestSubtraction(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
