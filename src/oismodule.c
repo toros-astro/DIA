0241d8ffb61fa3e1fa53def2817a35edb0c0554b
Martin Beroiz <martinberoiz@gmail.com>
2018-06-06 16:39:59 -0500

    C Ext rewrite

--- src/oismodule.c
+++ src/oismodule.c
@@ -1,5 +1,6 @@
 #include <Python.h>
-#include <numpy/arrayobject.h>
+#include <numpy/ndarrayobject.h>
+//#include <numpy/arrayobject.h>
 #include "oisdifference.h"
 
 #if PY_MAJOR_VERSION >= 3
@@ -9,32 +10,53 @@
 static PyObject *
 oismodule_subtract(PyObject *self, PyObject *args)
 {
-    PyArrayObject *np_sciimage, *np_refimage, *np_xc, *np_yc;
+
+    PyObject *py_sciobj, *py_refobj, *py_xcobj, *py_ycobj;
     int stamp_side;  // The stamp side 
     int kernel_side; // The kernel side in pixels
     int poly_deg; // The degree of the varying polynomial for the kernel
 
-    if (!PyArg_ParseTuple(args, "O!O!O!O!iii",
-        &PyArray_Type, &np_sciimage,
-        &PyArray_Type, &np_refimage,
-        &PyArray_Type, &np_xc,
-        &PyArray_Type, &np_yc,
+    if (!PyArg_ParseTuple(args, "OOOOiii",
+        &py_sciobj, &py_refobj, &py_xcobj, &py_ycobj,
         &kernel_side, &poly_deg, &stamp_side)) {
         return NULL;
     }
-    if (NULL == np_sciimage) return NULL;
-    if (NULL == np_refimage) return NULL;
-    if (NULL == np_xc) return NULL;
-    if (NULL == np_yc) return NULL;
 
-    double* sciimage = (double*)np_sciimage->data;
-    double* refimage = (double*)np_refimage->data;
-    int n = np_sciimage->dimensions[0];
-    int m = np_sciimage->dimensions[1];
+    PyObject *np_sciimage = PyArray_FROM_OTF(py_sciobj, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+    if (np_sciimage == NULL) {
+        Py_XDECREF(np_sciimage);
+        return NULL;
+    }
+    PyObject *np_refimage = PyArray_FROM_OTF(py_refobj, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+    if (np_refimage == NULL) {
+        Py_XDECREF(np_sciimage);
+        Py_XDECREF(np_refimage);
+        return NULL;
+    }
+    PyObject *np_xc = PyArray_FROM_OTF(py_xcobj, NPY_INT, NPY_ARRAY_IN_ARRAY);
+    if (np_sciimage == NULL) {
+        Py_XDECREF(np_sciimage);
+        Py_XDECREF(np_refimage);
+        Py_XDECREF(np_xc);
+        return NULL;
+    }
+    PyObject *np_yc = PyArray_FROM_OTF(py_ycobj, NPY_INT, NPY_ARRAY_IN_ARRAY);
+    if (np_refimage == NULL) {
+        Py_XDECREF(np_sciimage);
+        Py_XDECREF(np_refimage);
+        Py_XDECREF(np_xc);
+        Py_XDECREF(np_yc);
+        return NULL;
+    }
 
-    int nstars = np_xc->dimensions[0];
-    int* xc = (int *)np_xc->data;
-    int* yc = (int *)np_yc->data;
+    double* sciimage = (double*)PyArray_DATA(np_sciimage);
+    double* refimage = (double*)PyArray_DATA(np_refimage);
+    int n = (int)PyArray_DIM(np_sciimage, 0);
+    int m = (int)PyArray_DIM(np_sciimage, 1);
+
+    int nstars = (int)PyArray_DIM(np_xc, 0);
+    int* xc = (int *)PyArray_DATA(np_xc);
+    int* yc = (int *)PyArray_DATA(np_yc);
 
     image img = {sciimage, n, m};
     image ref = {refimage, n, m};
@@ -42,10 +64,16 @@ oismodule_subtract(PyObject *self, PyObject *args)
     double *subt = (double *)malloc(n * m * sizeof(*subt));
     perform_subtraction(ref, img, kernel_side / 2, stamp_side / 2, poly_deg, nstars, xc, yc, subt);
 
-    npy_intp diff_dims[2] = {n, m};
-    PyObject* py_conv_img = PyArray_SimpleNewFromData(2, diff_dims, NPY_DOUBLE, subt);
+    Py_DECREF(np_sciimage);
+    Py_DECREF(np_refimage);
+    Py_DECREF(np_xc);
+    Py_DECREF(np_yc);
 
-    return Py_BuildValue("O", py_conv_img);
+    npy_intp diff_dims[2] = {n, m};
+    PyArrayObject* py_subt_img = (PyArrayObject *)PyArray_SimpleNewFromData(2, diff_dims, NPY_DOUBLE, subt);
+    PyArray_ENABLEFLAGS(py_subt_img, NPY_ARRAY_OWNDATA);
+
+    return Py_BuildValue("N", py_subt_img);
 }
 
 static PyMethodDef OISModuleMethods[] = {
